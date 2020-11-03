2aa33cc4920cc0987a6706d66808c32da0ac5457
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:04:47 -0800

    config: Build QuickAccessWallet plugin for SystemUI
    This plugin that provides the wallet (cards and passes) integration in
    the power menu is now open-source, so let's build it.
    
    Change-Id: I5df8a09c8f91805df538bc7ddad6d326f5d9b13e

---
+++ config/packages.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) The ConquerOS Project
+#
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk
+
+# Root
+PRODUCT_PACKAGES += \
+    adb_root
+ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(WITH_SU),true)
+PRODUCT_PACKAGES += \
+    su
+endif
+endif
+
+# SystemUI plugins
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
+
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
