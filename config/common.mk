f8cff1e3de6a9555c644bf9c99044e5a5cbe099b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-07 15:37:23 -0300

    vendor: Add FaceUnlock support
    Change-Id: I361ce913dfb5f1db574b3cf3cdeeeaaca2338ac1

--- config/common.mk
+++ config/common.mk
@@ -32,6 +32,17 @@ endif
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/extended/overlay/common
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
