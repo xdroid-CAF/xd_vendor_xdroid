ConquerOS/vendor_conquer

vendor: import sdclang configs

config/common.mk
@@ -105,6 +105,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/conquer/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/conquer/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/conquer/config/aosp_audio.mk
 

sdclang/sdclang.mk
@@ -1,0 +1,15 @@
+# Copyright (C) 2016-2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SDCLANG_LTO_DEFS := vendor/conquer/sdclang/sdllvm-lto-defs.mk

sdclang/vendorsetup.sh
@@ -1,0 +1,2 @@
+# SDClang Environment Variables
+export SDCLANG_CONFIG=$(pwd)/vendor/conquer/sdclang/sdclang.json

xdroid-CAF/xd_vendor_xdroid

config/common.mk
@@ -106,6 +106,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/xdroid/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/xdroid/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/xdroid/config/aosp_audio.mk
 

sdclang/sdclang.mk
@@ -1,0 +1,15 @@
+# Copyright (C) 2016-2019 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SDCLANG_LTO_DEFS := vendor/xdroid/sdclang/sdllvm-lto-defs.mk

sdclang/vendorsetup.sh
@@ -1,0 +1,2 @@
+# SDClang Environment Variables
+export SDCLANG_CONFIG=$(pwd)/vendor/xdroid/sdclang/sdclang.json