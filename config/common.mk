ConquerOS/vendor_conquer

config: build common required packages
Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

config/common.mk
@@ -114,5 +114,8 @@ include vendor/conquer/config/conquer_audio.mk
 # Include ConquerUI
 include vendor/conquerui/config.mk
 
+# Include Common packages
+include vendor/conquer/config/packages.mk
+
 # Include Version
 include vendor/conquer/config/version.mk

xdroid-CAF/xd_vendor_xdroid

config/common.mk
@@ -115,5 +115,8 @@ include vendor/xdroid/config/xdroid_audio.mk
 # Include xdroidUI
 include vendor/xdroidui/config.mk
 
+# Include xdroid packages
+include vendor/xdroid/config/packages.mk
+
 # Include Version
 include vendor/xdroid/config/version.mk