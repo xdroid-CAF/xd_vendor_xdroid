f6be1b65f5a93be81593da23f64df7d98287706f
xyzuan <xyzuannihboss@gmail.com>
2021-08-25 17:06:40 +0700

    xdroid: vendorsetup: Add greeting msg.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,11 @@
 export CAF_REVISION_TAG=$(grep "default  revision" ".repo/manifests/default.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
 
+# Welcome Msg
+echo -e ==========================================
+echo 	 xd. xdroidOS Build Environtment
+echo -e ==========================================
+echo -e "xd. Revision: rev1.3.redwhite"
+echo -e "CAF Revision: $CAF_REVISION_TAG"
+
 # SDClang Environment Variables
 export SDCLANG_CONFIG=$(pwd)/vendor/xdroid/sdclang/sdclang.json
