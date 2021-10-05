bfe4f52f3e2d4d493fd30ede16434cf9eb5ad857
xyzuan <xyzuannihboss@gmail.com>
2021-10-05 20:55:24 +0700

    xdroid: bump `xd_rev1.6.redwhite-STBL`

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ export CAF_REVISION_TAG=$(grep "default  revision" ".repo/manifests/default.xml"
 echo -e ==========================================
 echo 	 xd. xdroidOS Build Environtment
 echo -e ==========================================
-echo -e "xd. Revision: rev1.5.redwhite"
+echo -e "xd. Revision: rev1.6.redwhite"
 echo -e "CAF Revision: $CAF_REVISION_TAG"
 
 # SDClang Environment Variables
